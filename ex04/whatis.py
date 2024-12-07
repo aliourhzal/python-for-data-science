4e230bd5b0f7427311f052297d23acda86260cb2
aliourhzal <aourhzal@student.1337.ma>
2024-12-07 19:29:00 +0100

    format code using flake8

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,21 +1,22 @@
 import sys
 
+
 def even_odd():
     args = sys.argv
     num_args = len(args)
 
     try:
         if num_args < 2:
-            return 
+            return
         if num_args > 2:
             raise AssertionError("more than one argument is provided")
         if not args[1].lstrip('+-').isdigit():
             raise AssertionError(" argument is not an integer")
-        
+
         num = int(args[1])
         print("I'm Even.") if num % 2 == 0 else print("I'm Odd.")
-    except Exception as e:
+    except AssertionError as e:
         print(f"AssertionError: {e}")
-    
+
 
 even_odd()
