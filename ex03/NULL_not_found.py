5db8675069308b1a0c16f8f2d603b3bb7344f8b9
aliourhzal <aourhzal@student.1337.ma>
2024-12-07 18:06:15 +0100

    handle nan

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -1,33 +1,18 @@
 
 def NULL_not_found(object: any) -> int:
-    if object:
+    obj_type = type(object)
+    if isinstance(object, float) and object != object:
+        print(f"Cheese: {object} {obj_type}")
+        return (0)
+    elif object:
+        print("Type not Found")
         return (1)
-    obj_type = object.__class__
     if object is None:
         print(f"Nothing: {object} {obj_type}")
-    elif obj_type is float:
-        print(f"Cheese: {object} {obj_type}")
-    elif obj_type is int:
-        print(f"Zero: {object} {obj_type}")
-    elif obj_type is str:
-        print(f"Empty: {object} {obj_type}")
-    elif obj_type is bool:
+    elif isinstance(object, bool):
         print(f"Fake: {object} {obj_type}")
-    else:
-        print("Type not Found")
+    elif isinstance(object, int):
+        print(f"Zero: {object} {obj_type}")
+    elif isinstance(object, str):
+        print(f"Empty: {object} {obj_type}")
     return (0)
-
-Nothing = None
-Garlic = float("NaN")
-Zero = 0
-Empty = ''
-Fake = False
-
-print(Garlic is None)
-
-# NULL_not_found(Nothing)
-# NULL_not_found(Garlic)
-# NULL_not_found(Zero)
-# NULL_not_found(Empty)
-# NULL_not_found(Fake)
-# print(NULL_not_found("Brian"))
