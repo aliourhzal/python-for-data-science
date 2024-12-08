637943ed9216e9a0ef1c1818f33b3eb1584aa0c3
aliourhzal <aourhzal@student.1337.ma>
2024-12-08 12:11:51 +0100

    print results and handle trailing new line

--- ex05/building.py
+++ ex05/building.py
@@ -12,7 +12,8 @@ def main():
         if len(sys.argv) > 2:
             raise AssertionError()
         elif len(sys.argv) < 2:
-            text = input("What is the text to count?\n")
+            print("What is the text to count?")
+            text = sys.stdin.readline()
         else:
             text = sys.argv[1]
 
@@ -25,9 +26,19 @@ def main():
                 digits += 1
             elif c.isspace():
                 spaces += 1
+            else:
+                punc += 1
+        print(f"The text contains {len(text)} characters:")
+        print(f"{upper} upper letters")
+        print(f"{lower} lower letters")
+        print(f"{punc} punctuation marks")
+        print(f"{spaces} spaces")
+        print(f"{digits} digits")
 
     except AssertionError:
         print(f"AssertionError: more than one argument is provided")
+    except Exception as e:
+        print(f"something went wrong: {e}")
 
 if __name__ == "__main__":
     main()
