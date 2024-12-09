25af18e78f62bdd2eaf2e8036042e4dbc554ec5c
aliourhzal <aourhzal@student.1337.ma>
2024-12-09 20:48:18 +0100

    implement apply_limit function

--- array/ex00/give_bmi.py
+++ array/ex00/give_bmi.py
@@ -7,8 +7,14 @@ def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int |
     bmi = w_arr / (h_arr ** 2)
     return list(bmi)
 
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    bmi_arr = np.array(bmi)
+    limit_arr = bmi_arr > limit
+    return (list(limit_arr)) 
+
 height = [2.71, 1.15]
 weight = [165.3, 38.4]
 
 bmi = give_bmi(height, weight)
 print(bmi, type(bmi))
+print(apply_limit(bmi, 26))
