fd43fa8dd8465b5f56d95944fd0e26bf1aee2f82
aliourhzal <aourhzal@student.1337.ma>
2024-12-11 10:22:00 +0100

    add a args checker function

--- array/ex00/give_bmi.py
+++ array/ex00/give_bmi.py
@@ -1,6 +1,22 @@
 import numpy as np
 
+def check_args(height: any, weight: any) -> bool:
+    if not isinstance(height, list) or not isinstance(weight,list):
+        return False
+    if len(height) != len(weight):
+        return False
+    valid_heights = all([(isinstance(item, int) or isinstance(item, float)) for item in height])
+    if not valid_heights:
+        return False
+    valid_weights = all([(isinstance(item, int) or isinstance(item, float)) for item in weight])
+    if not valid_weights:
+        return False
+    
+    return True
+
 def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    if not check_args(height=height, weight=weight):
+        return []
     h_arr = np.array(height)
     w_arr = np.array(weight)
 
