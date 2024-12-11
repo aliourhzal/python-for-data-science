4dd3d14ecc870fa97532684fdcbf9022c7e343ef
aliourhzal <aourhzal@student.1337.ma>
2024-12-11 11:37:44 +0100

    ex02

---
+++ array/ex02/load_image.py
@@ -0,0 +1,22 @@
+import numpy as np
+from PIL import Image
+import os
+
+
+def ft_load(path: str) -> np.array:
+    try:
+        if not path.endswith(".jpg") and not path.endswith(".jpeg"):
+            raise AssertionError("the provided file is not jpg nor jpeg")
+        img = Image.open(path)
+        img_arr = np.array(img)
+        print(f"The shape of image is: {img_arr.shape}")
+        return img_arr
+    except AssertionError as e:
+        print(f"AssertionError: {str(e)}")
+    except Exception as e:
+        print(f"error: {str(e)}")
+
+
+
+print(ft_load("landscape.jpg"))
+
