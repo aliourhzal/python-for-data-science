9e3cff468aac1335b4872aba8c325ac4cb489aa3
aliourhzal <aourhzal@student.1337.ma>
2024-12-11 10:23:11 +0100

    move tests to tester.py

--- array/ex00/give_bmi.py
+++ array/ex00/give_bmi.py
@@ -27,10 +27,3 @@ def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
     bmi_arr = np.array(bmi)
     limit_arr = bmi_arr > limit
     return (list(limit_arr)) 
-
-height = [2.71, 1.15]
-weight = [165.3, 38.4]
-
-bmi = give_bmi(height, weight)
-print(bmi, type(bmi))
-print(apply_limit(bmi, 26))
