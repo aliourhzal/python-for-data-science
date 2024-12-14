47bd38ca8e4e1a07be25d263844995bd62a0da81
aliourhzal <aourhzal@student.1337.ma>
2024-12-14 15:52:26 +0100

    turn number with symboles to real numbers

--- dataTable/ex02/aff_pop.py
+++ dataTable/ex02/aff_pop.py
@@ -1,6 +1,25 @@
 from load_csv import load
 from matplotlib import pyplot as plt
+import pandas as pn
 import numpy as np
+
+def to_number(data: pn.Series):
+    value = data.values[0]
+
+    if value[0].isalpha():
+        return data
+    
+    if value[-1].isdigit():
+         return int(value)
+
+    last_letter = value[-1].upper()
+
+    if last_letter == 'K':
+        return int(float(value[:-1]) * 1e3)        
+    elif last_letter == 'M':
+        return int(float(value[:-1]) * 1e6)
+    elif last_letter == 'B':
+        return int(float(value[:-1]) * 1e9)
 
 def main():
     # try:
@@ -8,23 +27,23 @@ def main():
         if data.empty:
             return
         
-        morocco_data = data.loc[data['country'] == 'Belgium']
+        belgium_data = data.loc[data['country'] == 'Belgium']
         france_data = data.loc[data['country'] == 'France']
+        france_data = france_data.apply(to_number, axis=0)
+        belgium_data = belgium_data.apply(to_number, axis=0)
         years = data.columns[1:252].to_numpy(dtype=int)
-        b_population = morocco_data.values[0][1:252]
+        b_population = belgium_data.values[0][1:252]
         f_population = france_data.values[0][1:252]
-        
-        print(f_population)
 
-        plt.plot(years, b_population, label='Belgium')
-        plt.plot(years, f_population, label='France')
-        plt.yticks(np.arange(0, 250, 20))
-        plt.xticks(np.arange(1800, 2050, 40))
-        plt.ylabel("Population")
-        plt.xlabel("Years")
-        plt.title("Population Projections")
-        plt.legend()
-        plt.show()
+        # plt.plot(years, b_population, label='Belgium')
+        # plt.plot(years, f_population, label='France')
+        # plt.yticks(np.arange(0, 250, 20))
+        # plt.xticks(np.arange(1800, 2050, 40))
+        # plt.ylabel("Population")
+        # plt.xlabel("Years")
+        # plt.title("Population Projections")
+        # plt.legend(loc=4)
+        # plt.show()
     # except Exception as e:
     #     print(f"unexpected error: {e}")
 
