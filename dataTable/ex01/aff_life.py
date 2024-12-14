19a9844b0bb47af1e18dd8326088ac62ad22d5c8
aliourhzal <aourhzal@student.1337.ma>
2024-12-14 11:22:59 +0100

    customize x axis labels and draw graph

--- dataTable/ex01/aff_life.py
+++ dataTable/ex01/aff_life.py
@@ -1,16 +1,21 @@
 import matplotlib.pyplot as plt
 from load_csv import load
+import numpy as np
 
 def main():
-    data = load("lex.csv")
-    morocco_data = data.loc[data['country'] == 'Morocco']
-    print(morocco_data)
-    keys = data.keys()
-    x_axis = keys[1:]
-    print(x_axis)
-    # plt.ylabel("life expectancy")
-    # plt.xlabel("Year")
+    data = load("lexd.csv")
+    if data.empty:
+        return 
+    morocco_row = data.loc[data['country'] == 'France']
+    morocco_data = morocco_row.values[0][1:]
+    years = morocco_row.columns[1:].to_numpy()
 
+    plt.xticks(np.arange(0, 300, 40))
+    plt.ylabel("Life expectancy")
+    plt.xlabel("Year")
+    plt.title("Morocco Life expectancy Projections")
+    plt.plot(years, morocco_data)
+    plt.show()
 
 if __name__ == "__main__":
     main()
