5ccac4d32942beaa5731fd79cafc59658e86ef8a
aliourhzal <aourhzal@student.1337.ma>
2024-12-15 13:03:41 +0100

    morocco vs france and draw plot

--- dataTable/ex02/aff_pop.py
+++ dataTable/ex02/aff_pop.py
@@ -1,9 +1,9 @@
 from load_csv import load
 from matplotlib import pyplot as plt
-import pandas as pn
+import pandas as pd
 import numpy as np
 
-def to_number(data: pn.Series):
+def to_number(data: pd.Series):
     value = data.values[0]
 
     if value[0].isalpha():
@@ -27,23 +27,23 @@ def main():
         if data.empty:
             return
         
-        belgium_data = data.loc[data['country'] == 'Belgium']
+        morocco_data = data.loc[data['country'] == 'Morocco']
         france_data = data.loc[data['country'] == 'France']
         france_data = france_data.apply(to_number, axis=0)
-        belgium_data = belgium_data.apply(to_number, axis=0)
+        morocco_data = morocco_data.apply(to_number, axis=0)
         years = data.columns[1:252].to_numpy(dtype=int)
-        b_population = belgium_data.values[0][1:252]
+        b_population = morocco_data.values[0][1:252]
         f_population = france_data.values[0][1:252]
 
-        # plt.plot(years, b_population, label='Belgium')
-        # plt.plot(years, f_population, label='France')
-        # plt.yticks(np.arange(0, 250, 20))
-        # plt.xticks(np.arange(1800, 2050, 40))
-        # plt.ylabel("Population")
-        # plt.xlabel("Years")
-        # plt.title("Population Projections")
-        # plt.legend(loc=4)
-        # plt.show()
+        plt.plot(years, b_population, label='Morocco', color='r')
+        plt.plot(years, f_population, label='France', color='k')
+        plt.yticks(np.arange(20*1e6,80*1e6, 20*1e6), ['20M', '40M', '60M'])
+        plt.xticks(np.arange(1800, 2050, 40))
+        plt.ylabel("Population")
+        plt.xlabel("Years")
+        plt.title("Population Projections")
+        plt.legend(loc=4)
+        plt.show()
     # except Exception as e:
     #     print(f"unexpected error: {e}")
 
