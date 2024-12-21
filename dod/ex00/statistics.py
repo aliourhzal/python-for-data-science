43359a56e5b513e58c0207571b3ebc9292e80fa8
aliourhzal <aourhzal@student.1337.ma>
2024-12-21 16:03:49 +0100

    better logic less code

--- dod/ex00/statistics.py
+++ dod/ex00/statistics.py
@@ -8,12 +8,12 @@ def median(*args: any):
     is_even = len(args) % 2 == 0
 
     sorted_args = sorted(args)
+    size = len(sorted_args)
 
     if not is_even:
-        index =  len(sorted_args) // 2 
-        return sorted_args[index]
+        return sorted_args[size // 2]
     else:
-        min_mid = len(sorted_args) // 2 - 1
+        min_mid = size // 2 - 1
         return (sum(sorted_args[min_mid:min_mid + 2]) / 2)
     
 def quartile(*args: any):
@@ -23,18 +23,11 @@ def quartile(*args: any):
     first_quarter = sorted_args[0:len(sorted_args) // 2]
     lower_quartile = first_quarter[len(first_quarter) // 2]
 
-    if is_even:
-        last_quarter = sorted_args[len(sorted_args) // 2:]
-    else:
-        last_quarter = sorted_args[len(sorted_args) // 2 + 1:]
+    reversed_sorted = sorted(args, reverse=True)
+    last_quarter = reversed_sorted[0:len(reversed_sorted) // 2]
+    upper_quartile = last_quarter[len(last_quarter) // 2]
 
-    quarter_is_even = len(last_quarter) % 2 == 0
-
-    if quarter_is_even:
-        upper_quartile = last_quarter[len(last_quarter) // 2 - 1]
-    else:
-        upper_quartile = last_quarter[len(last_quarter) // 2]
-    return (float(lower_quartile), float(upper_quartile))
+    return [float(lower_quartile), float(upper_quartile)]
 
 def var(*args: any):
     data_mean = mean(*args)
